6308157dbbee16565e709e321071c272d6b54703
MonaLisa <1214@holbertonschool.com>
2019-12-13 03:09:19 +0000

    [Fix]: Syntax errors

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,17 +3,18 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv, exit
 
-    dic = {"+":add, "-":sub, "*":mul, "/":div}
+    op = {"+":add, "-":sub, "*":mul, "/":div}
 
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
-    for n in dic:
-        if argv[2] in dic:
-            if n == argv[2]:
-                dic = dic[n]
+    if argv[2] in op:
+        for n in op:
+            if argv[2] == n:
+                op = op[n]
                 break
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-    print("{}".format(dic(int(argv[1]), int(argv[3]))))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    print("{} {} {} = {}".format(argv[1], n, argv[3],
+                                 op(int(argv[1]), int(argv[3]))))
