f3a584b1ee4ef1dee7e1a845fdbb8683a858b48b
MonaLisa <1214@holbertonschool.com>
2019-12-13 02:19:02 +0000

    [Fix]: Condition if executed when imported

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
-sum = 0
-if len(argv) == 1:
-    print("{:d}".format(0))
-else:
-    for n in range(1, len(argv)):
-        sum += int(argv[n])
-    print("{}".format(sum))
+if __name__ == "__main__":
+    from sys import argv
+    sum = 0
+    if len(argv) == 1:
+        print("{:d}".format(0))
+    else:
+        for n in range(1, len(argv)):
+            sum += int(argv[n])
+        print("{}".format(sum))
