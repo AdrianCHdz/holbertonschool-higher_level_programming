031e7b8e34742f13e3b0d30dd16b90603dbb5a53
MonaLisa <1214@holbertonschool.com>
2019-12-13 03:10:09 +0000

    [Fix]: Syntax errors

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv, exit
 
-    op = {"+":add, "-":sub, "*":mul, "/":div}
+    op = {"+": add, "-": sub, "*": mul, "/": div}
 
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
