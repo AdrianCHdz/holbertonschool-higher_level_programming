aca8e7df9de928bbbd9b3cdef3215f357da1e76c
AdnxCHdz <1214@holbertonschool.com>
2019-12-17 21:25:24 -0500

    [fix]: Handling when out of range

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    my_list[idx] = element if ((len(my_list)) > idx >= 0) else (
-        my_list[idx])
+    if ((len(my_list)) > idx >= 0):
+        my_list[idx] = element
     return my_list
