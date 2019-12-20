8c1893a1df9e05efeda9dabd00f6ee44776677cd
AdnxCHdz <1214@holbertonschool.com>
2019-12-19 23:32:09 -0500

    [Fix]: Removing extra character

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return ([a for a in set_1 if a not in set_2] +\
+    return ([a for a in set_1 if a not in set_2] +
             [a for a in set_2 if a not in set_1])
