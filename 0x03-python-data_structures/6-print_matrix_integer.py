6df7ad26576a74d326f6b4f7e538ff404f2a08b2
AdrianCHdz <1214@holbertonschool.com>
2020-01-03 20:37:04 +0000

    [Fix]: in case of matrix being Null

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for n in i:
-            print("{}".format(n), end= '\n' if n % 3 == 0 else ', ')
+            print("{}".format(n), end='' if n == i[len(i) - 1] else ' ')
+        print()
