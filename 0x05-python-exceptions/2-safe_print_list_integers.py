571499921f1d3977fb73b3e320d5a8b16e665eb0
AdnxCHdz <1214@holbertonschool.com>
2020-01-08 17:21:57 -0500

    [Feature]: Adding second possible way

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    a = 0
     for n in range(x):
         try:
-            n
-            n
             print("{:d}".format(my_list[n]), end="")
+            a += 0
         except (ValueError, TypeError):
-            n -= 2
             pass
     print()
-    return (n + 1)
+    return (a)
 '''
         print(*([my_list[n] for n in range(x)]), sep="")
 '''
