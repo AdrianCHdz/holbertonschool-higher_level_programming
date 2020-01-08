357a052e722a3545813dac7399ac52e3a6b86350
AdnxCHdz <1214@holbertonschool.com>
2020-01-08 17:33:19 -0500

    [Fix]: Iteration over the list length

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,11 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     for n in range(x):
         try:
             print("{:d}".format(my_list[n]), end="")
-            a += 0
+            a += 1
         except (ValueError, TypeError):
             continue
     print()
     return a
-'''
-        print(*([my_list[n] for n in range(x)]), sep="")
-'''
