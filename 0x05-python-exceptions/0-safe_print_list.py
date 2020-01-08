05873bcd998f5ce0f678c5665278543da57c2cba
AdnxCHdz <1214@holbertonschool.com>
2020-01-08 17:21:09 -0500

    [Feature]:Adding second possible way

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    a = 0
     for n in range(x):
         try:
             print("{}".format(my_list[n]), end="")
+            a += 1
         except IndexError:
-            n = n - 1
             break
     print()
-    return(n + 1)
+    return(a)
 '''
         print(*([my_list[n] for n in range(x)]), sep="")
 '''
