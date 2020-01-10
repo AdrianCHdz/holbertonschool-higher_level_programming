1522fcfef6777f9ecffebbc05651e61086f5ef3e
AdnxCHdz <1214@holbertonschool.com>
2020-01-09 21:37:10 -0500

    [Feature]: Adding file with the function of inizialating an instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """An empty class that defines a square"""
+    def __init__(self, size='0'):
+        """Initialization of the method"""
+        self.__size = size
