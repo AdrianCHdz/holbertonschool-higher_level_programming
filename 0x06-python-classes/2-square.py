34b6a1fcec053e3b4b2a4185124b6d5a276deea9
AdnxCHdz <1214@holbertonschool.com>
2020-01-10 09:20:59 -0500

    [Fix]: PEP8 style guide

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,7 +3,7 @@ class Square:
     """An empty class that defines a square"""
     def __init__(self, size=0):
         """inizialization and conditioning input only to be integer"""
-        if isinstance(size, int) == False:
+        if isinstance(size, int) is False:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
