054fc300174d4605e72828029ba9dc2b0ccb8cd6
AdnxCHdz <1214@holbertonschool.com>
2020-01-09 21:42:36 -0500

    [Fix]: Pep8 style guide errors

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,12 +3,10 @@ class Square:
     """An empty class that defines a square"""
     def __init__(self, size=0):
         """inizialization and conditioning input only to be integer"""
-        '''
         if isinstance(size, int) == False:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        '''
         self.__size = size
 
     def area(self):
@@ -31,11 +29,11 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-            '''
-            qube = ["#" for n in range(self.__size)]
-            print(*(["".join(qube) for n in range(self.__size)]), sep="\n")
-            '''
         for x in range(self.__size):
             for y in range(self.__size):
                 print("#", end="")
             print()
+        '''
+        qube = ["#" for n in range(self.__size)]
+        print(*(["".join(qube) for n in range(self.__size)]), sep="\n")
+        '''
