4839f9f75f00d8df8fd2e892911c4bb200fd64e6
AdnxCHdz <1214@holbertonschool.com>
2020-01-09 21:51:19 -0500

    [Feature]: Adding position instance to the class

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+class Square:
+    """An empty class that defines a square"""
+    def __init__(self, size=0, position=(0, 0)):
+        """inizialization and conditioning input only to be integer"""
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """returns the area of the Square object"""
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """Setting of getter that will get the private attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        self.__position = value
+
+    def my_print(self):
+        if self.__size == 0 or self.position[1]:
+            print()
+            '''
+            qube = ["#" for n in range(self.__size)]
+            print(*(["".join(qube) for n in range(self.__size)]), sep="\n")
+            '''
+        for x in range(self.__size):
+            for z in range(self.position[0]):
+                print(" ", end="")
+            for y in range(self.__size):
+                print("#", end="")
+            print()
