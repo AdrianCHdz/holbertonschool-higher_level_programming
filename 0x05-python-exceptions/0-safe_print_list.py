aac435b3b28c9379525dd0fe044ab97a8f020e69
AdnxCHdz <1214@holbertonschool.com>
2020-01-10 16:51:02 -0500

    [Feature]: Adding possible ways of doing so

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,6 +9,7 @@ def safe_print_list(my_list=[], x=0):
             break
     print()
     return(a)
-'''
-        print(*([my_list[n] for n in range(x)]), sep="")
-'''
+#    print(*(my_list[n] for n, m in zip(range(x), my_list)))
+#    print(*([my_list[n] for n, m in zip(range(x), my_list)]), sep="")
+#    return sum(1 for n in zip(range(x), my_list))
+#    return sum(map(lambda x: 1, zip(range(x), my_list)))
