0d7335d3810ee72e81a38c163272bd8710a5b45c
AdnxCHdz <1214@holbertonschool.com>
2020-01-15 00:46:24 -0500

    [fix]: syntax errors

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -49,14 +49,14 @@ class Rectangle:
         grid = "\n".join(
             [str(self.print_symbol) * self.__width] * self.__height)
         return grid
-
+    """
     def __repr__(self):
         return ("Rectangle({}, {})".format(self.width, self.height))
 
     def __del__(self):
         print("Bye rectangle...")
         self.__class__.number_of_instances -= 1
-    """
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if isinstance(rect_1, Rectangle) is False:
