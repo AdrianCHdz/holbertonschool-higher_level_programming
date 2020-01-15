82385c38b4525faa8f0d53f5cd36fdd9e2e7e7da
AdnxCHdz <1214@holbertonschool.com>
2020-01-14 22:27:08 -0500

    [Feature]: File that defines area of rectangel

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        perim = (self.__width * 2) + (self.__height * 2)
+        if self.__width is 0 or self.__height is 0:
+            perim = 0
+        return perim
