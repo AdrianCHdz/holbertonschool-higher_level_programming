2a286f29b6937ff283b05bc0d45896d2835641ae
AdrianCHdz <1214@holbertonschool.com>
2020-08-10 04:47:04 +0000

    im just dumb

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,13 +6,11 @@ from sys import argv
 from MySQLdb import connect
 
 if __name__ == '__main__':
-
     init = connect(user=argv[1], passwd=argv[2],
                    db=argv[3], host="localhost", port=3306)
     query = init.cursor()
     state = argv[4]
-    print(state)
-    search = "SELECT * FROM states WHERE name= '{}' ORDER BY states.id"
+    search = "SELECT * FROM states WHERE name='{}' ORDER BY states.id ASC"
     search = search.format(state)
     query.execute(search)
 
