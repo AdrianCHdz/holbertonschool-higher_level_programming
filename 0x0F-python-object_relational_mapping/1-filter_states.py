55307546129df9a7c5e1245e90691bd97e57e076
AdrianCHdz <1214@holbertonschool.com>
2020-08-10 04:31:18 +0000

    pep8 identation syntax error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,9 +9,11 @@ if __name__ == '__main__':
     """
     doc
     """
-    init = connect(user=argv[1], passwd=argv[2], db=argv[3], host="localhost", port=3306)
+    init = connect(user=argv[1], passwd=argv[2],
+                   db=argv[3], host="localhost", port=3306)
     query = init.cursor()
-    query.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+    query.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY
+    states.id""")
     results = query.fetchall()
     for row in results:
         print(row)
