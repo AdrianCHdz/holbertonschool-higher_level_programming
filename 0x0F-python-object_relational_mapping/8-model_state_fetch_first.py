81744f70e9d1ccefd94dcef137bcc5a625d7598e
AdrianCHdz <1214@holbertonschool.com>
2020-08-10 05:04:27 +0000

    this error costed me a lot :)

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+doc
+"""
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
@@ -9,7 +11,6 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
