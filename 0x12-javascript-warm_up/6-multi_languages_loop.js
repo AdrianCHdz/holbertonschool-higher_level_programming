26a58ba9710d81372bdff632234c083b8c0e9242
Ubuntu <ubuntu@ip-172-31-23-206.us-east-2.compute.internal>
2020-09-15 19:31:49 +0000

    [Feature]: Js script that prints a 3 lines with a for loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let x = 0; x < args.length; x++) {
+  console.log(args[x]);
+}
+/*
+  args.forEach(value => {
+console.log(value);
+  });
+*/
